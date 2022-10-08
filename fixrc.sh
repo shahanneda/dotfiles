fade38d55b29b4af88e712204e95332b85293e85
shahanneda <shahan.neda@gmail.com>
2022-10-07 23:28:07 -0400

    Fixed comment for linux systems

--- fixrc.sh
+++ fixrc.sh
@@ -1,5 +1,5 @@
 ln -s -f ~/dotfiles/.zshrc ~/.zshrc
 ln -s -f ~/dotfiles/.vimrc ~/.vimrc
 ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
-// have to run tmux, then Cntrl+B :source-file ~/.tmux.conf
+#// have to run tmux, then Cntrl+B :source-file ~/.tmux.conf
 
