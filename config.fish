80e4b054ff91342268514a43a28c25924c41f2e0
Shahan Neda <shahan.neda@gmail.com>
2023-08-05 18:40:03 -0400

    Added to script

--- config.fish
+++ config.fish
@@ -1,2 +1,9 @@
 nvm use latest
 alias gac "git add --all && git commit"
+alias grc "git add --all && git rebase --continue"
+alias nrd "npm run dev"
+alias pj "cd ~/Documents/projects"
+alias school "~/Documents/classes"
+alias f23 "~/Documents/classes/F23"
+alias ovac "cd ~/Documents/Vault/ && git add --all && git commit -m \"Notes\" && git push"
+
--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 git config --global core.editor "vim"
+git config --global user.name "Shahan Neda"
+git config --global user.email shahan.neda@gmail.com
+
 sudo dnf install konsole
 sudo dnf install fish
+sudo dnf install screenfetch
 chsh fish
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher install jorgebucaran/nvm.fish
 nvm install latest
 nvm use latest
+mkdir ~/Documents/projects
+cd ~/Documents
+git clone git@github.com:shahanneda/Vault.git
