abb72fdae06244bb709ab9d9fdd3b8fbcfe7f6a5
Shahan Neda <shahan.neda@gmail.com>
2023-08-04 22:46:13 -0400

    moved to script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
 git config --global core.editor "vim"
+sudo dnf install konsole
+sudo dnf install fish
+chsh fish
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install jorgebucaran/nvm.fish
+nvm install latest
+nvm use latest
