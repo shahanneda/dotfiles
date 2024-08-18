c486e3043c42d137a038b8a13bb6c4360b6fee5f
Shahan Neda <shahan.neda@gmail.com>
2024-08-17 17:25:04 -0700

    Scheduled commit

--- config.fish
+++ config.fish
@@ -29,7 +29,8 @@ fish_add_path ~/.cargo/bin
 if test (uname) = "Darwin"
     pyenv init - | source
 end
-python ~/Documents/projects/syncer/check_last_sync.py
+set test 
+python realpath ~/Documents/projects/syncer/check_last_sync.py
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/home/shahanneda/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/home/shahanneda/Downloads/google-cloud-sdk/path.fish.inc'; end
