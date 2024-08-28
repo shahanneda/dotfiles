34700b85187eab9a7582b0a8804db6456b7c4d1b
Shahan Neda <shahan.neda@gmail.com>
2024-08-27 20:40:07 -0700

    Added go links

--- fixrc.sh
+++ fixrc.sh
@@ -1,6 +1,8 @@
 ln -s -f ~/dotfiles/.zshrc ~/.zshrc
 ln -s -f ~/dotfiles/.vimrc ~/.vimrc
 ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s -f ~/dotfiles/go_links.js ~/Documents/projects/go-links/constants.js
+    
 mkdir ~/vimtemp/
 chmod 777 ~/vimtemp//
 # rm /home/shahanneda/.config/fish/config.fish Not sure why this was here! but keeping it incase its needed
---
+++ go_links.js
@@ -0,0 +1,16 @@
+export const DEFAULT_GO_LINKS = {
+    "cal": "https://calendar.google.com",
+    "docs": "https://docs.google.com",
+    "drive": "https://drive.google.com",
+    "extensions": "chrome://extensions/",
+    "github": "https://github.com/",
+    "mail": "https://mail.google.com/",
+    "go": "chrome://extensions/?id=obphcibpljohboodikbpckofphjdljog",
+    "hn": "https://news.ycombinator.com/",
+    "x": "https://x.com/home",
+    "c": "https://shahan.ca/clip",
+    "dotfiles": "https://github.com/shahanneda/dotfiles",
+    "fck": "https://github.com/uwblueprint/feeding-canadian-kids",
+    "fcktodo": "https://www.notion.so/uwblueprintexecs/What-s-Left-a7eff292cb1f438d85762285c02e50c0",
+    "llm": "https://claude.ai/new"
+  };
